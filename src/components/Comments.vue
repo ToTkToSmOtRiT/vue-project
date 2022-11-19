8e79267451a48709c6022646d3b75b8dfe8f4882
Marshall <zz90847@gmail.com>
2022-11-19 13:42:15 +0300

    modified:   src/components/Comments.vue

--- src/components/Comments.vue
+++ src/components/Comments.vue
@@ -1,12 +1,21 @@
 <template>
-
+    <ol>
+        <li></li>
+    </ol>
 </template>
 
+
 <script>
-    export default{
-        name: 'Comms'
+export default {
+    name: 'Comms',
+    data(){
+        return{
+            comment: ''
+        }
     }
+}
 </script>
+
 
 <style>
 
